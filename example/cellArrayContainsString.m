b2c05eaff9452f74614dc66e75894ec10583a319
anon <gitdev@web.de>
2016-07-22 10:15:37 +0200

    added README.md

--- example/cellArrayContainsString.m
+++
@@ -1,8 +0,0 @@
-function contains = cellArrayContainsString(cellArray, string)
-    contains = 0;
-    for i=1:length(cellArray)
-        if (strcmp(cellArray{i},string) == 1) 
-            contains = 1;
-        end
-    end
-end
--- example/main.m
+++
@@ -1,6 +0,0 @@
-setup_nctoolbox;
-
-latitude  = 54;
-longitude =  8;
-nameOfCSVFile = 'csvDump.csv';
-grb2ToCSV(latitude, longitude, nameOfCSVFile);
